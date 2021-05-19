66427e1670758c3892fd4bdf78813ce4d1b28e58
oiji-official <xyz.notimportant@gmail.com>
2021-05-20 05:14:25 +0530

    added 2 blocks (first commit)

---
+++ blockchain-ver1.js
@@ -0,0 +1,43 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+    
+    calculateHash(){
+        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+    
+    createGenesisBlock(){
+        return new Block(0, "20-05-2021", "Genesis Block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[(this.chain.length) - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let myCoin = new Blockchain();
+myCoin.addBlock(new Block(1, "21-05-21", {amount: 11}));
+myCoin.addBlock(new Block(2, "22-05-21", {amount: 36}));
+
+console.log(JSON.stringify(myCoin, null, 4));
+
+
